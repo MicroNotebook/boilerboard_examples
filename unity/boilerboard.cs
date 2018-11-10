4b14635370af0e43fd9c2be9a0e790bb975a00a8
Carlo Supina <cdsupina@gmail.com>
2018-11-10 05:24:12 -0500

    add cs unity script

---
+++ unity/boilerboard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class boilerboard : MonoBehaviour {
+
+    SerialPort stream = new SerialPort("COM3", 115200);
+	// Use this for initialization
+	void Start () {
+        stream.ReadTimeout = 1;
+        stream.Open();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        try {
+            Debug.Log("Read " + stream.ReadChar());
+        }
+        catch {
+            //Debug.Log("error");
+        }
+    }
+}
